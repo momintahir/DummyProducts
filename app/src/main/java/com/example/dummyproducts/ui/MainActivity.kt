0f031f298756c674a5355c6affaac6441ad32969
Momin Tahir <momintahir7@gmail.com>
2021-09-16 23:39:43 +0500

    Project structuring and adding dependencies

--- app/src/main/java/com/example/dummyproducts/ui/MainActivity.kt
+++ app/src/main/java/com/example/dummyproducts/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.dummyproducts
+package com.example.dummyproducts.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dummyproducts.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
