4c7794f66f473c448c3a6c5b93e98586f9a4b12f
Momin Tahir <momintahir7@gmail.com>
2021-09-17 00:00:36 +0500

    Setting-up-navigation

--- app/src/main/java/com/example/dummyproducts/ui/MainActivity.kt
+++ app/src/main/java/com/example/dummyproducts/ui/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.dummyproducts.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.dummyproducts.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.productsNavHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
